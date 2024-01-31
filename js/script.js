83155d0342f236c9e821045eb23fe704047619d0
mavmis <malovs769@gmail.com>
2024-01-31 13:00:23 +0300

    21

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var count_tables = 0;
 function Setbutton(){ // создание формы для ввода строк и стобцов
     // let TableElement = document.querySelector('button.AddTableRowsCols');
 
@@ -26,13 +27,15 @@ function default_event(event) {
 }
 
 function generateTable(){ // чтение данных из формы
+    count_tables += 1;
     const form = document.querySelector('form.form_SetRowCols'); // выбор формы для последующего чтения из него данных
     form.addEventListener('submit', default_event); // установка события для submit
 
     const TableCol = form.querySelector('[name="Col"]'), TableRow = form.querySelector('[name="Row"]'); // чтение данных из формы
     const data = {
         row: TableRow.value,
-        col: TableCol.value
+        col: TableCol.value,
+        id: count_tables
     }; // создание object с row и col
 
     createTable(data); // вызов функции createTable
@@ -40,10 +43,10 @@ function generateTable(){ // чтение данных из формы
 
 function createTable(data){ // создание исходной таблицы от данных data
     let table_section = document.createElement('section'); //создание блока где будет лежать table
-    table_section.id = "table_section"; // установка id 
+    table_section.id = "table_section_"+count_tables; // установка id 
 
     let table = document.createElement('table'); // создание table
-    table.id = 'table'; // установка id
+    table.id = "table_"+count_tables; // установка id
 
     table_section.append(table); // вставка таблицы в блок section
     SetRowCol.after(table_section); // вставка table_section после секции SetRowCol
@@ -52,35 +55,45 @@ function createTable(data){ // создание исходной таблицы 
         createRow(data);
         data.row--;
     }
-
+    
     GenerateButtons(data); // Вызов функции GenerateButtons
 }
 
 function GenerateButtons(data){ // создание buttons для добавления строк и столбцов в table
-
+    let section = document.querySelector("#table_section_"+count_tables);
     let GenerateButtons = document.createElement("div"); // создание блока div
-    GenerateButtons.id = "GenBut"
-    GenerateButtons.innerHTML = `
-    <input type="submit" id ="createRow" value="Добавить строку"/>
-    <input type="submit" id ="createCol" value="Добавить столбец"/>
-    `; // изменение содержимого html внутри div
 
-    table_section.append(GenerateButtons); // вставка div после table_section (id)
+    GenerateButtons.id = "GenBut_"+count_tables;
+    let inputRow = document.createElement('input');
+    inputRow.id = "createRow_"+count_tables;
+    inputRow.value="Добавить строку";
+    inputRow.type="submit";
 
-    let row = document.querySelector("#createRow"); // выбор по id createRow
-    row.onclick  = createRow.bind(row, data); // установка вызова функции при нажатии
+    let inputCol = document.createElement('input');
+    inputCol.id = "createCol_"+count_tables;
+    inputCol.value="Добавить столбец";
+    inputCol.type="submit";
+    
 
-    let col = document.querySelector("#createCol"); // выбор по id createCol
-    col.onclick  = createCol.bind(col, data); // установка вызова функции при нажатии
+    GenerateButtons.append(inputRow);
+    GenerateButtons.append(inputCol);
+
+    section.append(GenerateButtons);// вставка div после table_section (id)
+
+    let row = document.querySelector("#createRow_"+count_tables); // выбор по id createRow
+    row.onclick  = createRow.bind(row, data);
+
+    let col = document.querySelector("#createCol_"+count_tables); // выбор по id createCol
+    col.onclick  = createCol.bind(col, data);
 }
 
 function createRow(data){ // добавление строки
-    let table = document.querySelector('#table');
-    let tr = document.createElement('tr');
+    let table = document.querySelector("#table_"+data.id);
+    let tr = document.createElement("tr");
     let td;
 
     for (let i = 0; i < data.col; i++) {
-        td = document.createElement('td');
+        td = document.createElement("td");
         td.innerHTML = `<input type="text" class="table_text">`;
         tr.append(td);
     }
@@ -90,7 +103,9 @@ function createRow(data){ // добавление строки
 }
 
 function createCol(data){ // добавление столбца
-    let trs = document.querySelectorAll('#table tr');
+    console.log("table_"+count_tables+" tr", data.col, data.row);
+    let trs = document.querySelectorAll("#table_"+data.id+" tr");
+    console.log(trs);
     let td;
 
     for (let tr of trs) {
